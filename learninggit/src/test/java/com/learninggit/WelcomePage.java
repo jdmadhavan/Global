6ec6ea1db224aad234384407fd9e80fc13a2c43b
madhavanramlingam@gmail.com <madhavan_r@CHD122908.photonint.com>
2019-09-26 13:28:50 +0530

    Second Commit from Local Repo

--- learninggit/src/test/java/com/learninggit/WelcomePage.java
+++ learninggit/src/test/java/com/learninggit/WelcomePage.java
@@ -3,7 +3,7 @@ package com.learninggit;
 public class WelcomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcom to page");
 
 	}
 
