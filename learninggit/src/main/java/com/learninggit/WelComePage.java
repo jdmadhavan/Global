3ae021515bb4cf8650cd7862a629c9f2403201a8
madhavan <madhavanramlingam@gmail.com>
2024-05-20 09:25:07 +0530

    sprint1changes

---
+++ learninggit/src/main/java/com/learninggit/WelComePage.java
@@ -0,0 +1,12 @@
+package com.learninggit;
+
+public class WelComePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Neewly add development");
+
+	}
+
+}
